306b021c1a5604dffabbf478e9b7070942abbbb0
Peter Dubec <peter.dubec93@gmail.com>
2016-01-27 12:09:11 +0100

    checks dates validity

--- rpc.rb
+++ rpc.rb
@@ -1,7 +1,9 @@
+require 'date'
+
 class FormatChecker
   def self.check_regex_format(value, type, length)
     case type
-      when 'N', 'R' #regex checking numeric characters
+      when 'N', 'R', 'D' #regex checking numeric characters
         return true if value == value.match(/[0-9]{#{length}}/).to_s
 
       when 'A' #regex checking alphanumeric characters (right padded with spaces)
@@ -35,17 +37,19 @@ class FileChecker
   #   N = numeric
   #   A = alphanumeric
   #   R = row_number(numeric)
+  #   D = date(numeric)
 
   HEADER_FORMAT = %w(NNN NNNNNNNNNN NN NNNNNNNNNNNN AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA NNNNNNNNNNNNNNN NNNNNN
                      NNNN RRRRRR NNN AAAAAAAAA)
 
-  DETAIL_FORMAT = %w(NNN NNNNNNNNNN NN NNNNNNNN NNNN NNN NNNNNNNN AAAAAAAAAAAAAAAAAAAA NNN
+  DETAIL_FORMAT = %w(NNN NNNNNNNNNN NN DDDDDDDD NNNN NNN DDDDDDDD AAAAAAAAAAAAAAAAAAAA NNN
                      NNNNNNNNNNNNNNN NNNNNN AAA AAAA RRRRRR AAAAAAAAAA)
 
   TRAILER_FORMAT = %w(NNN NNNNNNNNNN NN NNNNNNNNNNNN AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA NNNNNNNNNNNNNNN
                       NNNNNN RRRRRR AAAAAAAAAA)
 
   ROW_NUMBERS_LENGTH = 6
+  DATE_LENGTH = 8
 
   # computes length of predefined format
   def self.compute_length(format)
@@ -55,6 +59,8 @@ class FileChecker
   end
 
   # checks row numbering
+
+
   def self.check_row_numbering(line,number,format, type)
     #check if format contains R's = rows are numbered and if numbering is correct
     format = format.join('')
@@ -69,6 +75,39 @@ class FileChecker
     end
   end
 
+  # extracts dates based on predefined format
+  def self.extract_dates(line, format, type)
+    format = format.join('')
+    index = format.index('D')
+    dates = []
+
+    while index do
+      if format[index,DATE_LENGTH] == "D" * DATE_LENGTH
+        dates << line[index,DATE_LENGTH]
+        line = line[index+DATE_LENGTH..-1]
+        format = format[index+DATE_LENGTH..-1]
+        index = format.index('D')
+      else
+        puts "Format of #{type} does not have date correctly defined"
+        break
+      end
+    end
+    return dates
+  end
+
+  def self.check_dates(dates,number)
+    dates.each do |date|
+      date = date.to_s
+      year = date[4,4].to_i
+      month = date[2,2].to_i
+      day = date[0,2].to_i
+      if Date.valid_date?(year, month, day) == false
+       puts "Date #{day}/#{month}/#{year} on line #{number} is not valid."
+      end
+
+    end
+  end
+
   # iterates through whole file and checks all lines
   file = File.open("#{ARGV[0]}", 'r')
   file.each_with_index do |line, index|
@@ -76,13 +115,16 @@ class FileChecker
     if index == 0 # HEADER
       FormatChecker.check_format(line, compute_length(HEADER_FORMAT), HEADER_FORMAT, "HEADER format is invalid")
       check_row_numbering(line, index, HEADER_FORMAT, "HEADER")
+      check_dates(extract_dates(line, HEADER_FORMAT, "HEADER"),index)
     else
       if file.eof? # TRAILER
         FormatChecker.check_format(line, compute_length(TRAILER_FORMAT), TRAILER_FORMAT, "TRAILER format is invalid")
         check_row_numbering(line, index, TRAILER_FORMAT, "TRAILER")
+        check_dates(extract_dates(line, TRAILER_FORMAT, "TRAILER"),index)
       else # DETAIL
         FormatChecker.check_format(line, compute_length(DETAIL_FORMAT), DETAIL_FORMAT, "Format on line #{index} is invalid")
         check_row_numbering(line, index, DETAIL_FORMAT, "DETAIL")
+        check_dates(extract_dates(line, DETAIL_FORMAT, "DETAIL"),index)
       end
     end
   end
